eea179efefaded133f7eaf1a1d2049576b057ad3
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2023-08-19 23:23:19 -0600

    add navbar and terminal-like styling

--- src/components/pages/about.rs
+++ src/components/pages/about.rs
@@ -3,9 +3,9 @@ use yew::prelude::*;
 #[function_component(About)]
 pub fn about() -> Html {
     html! {
-    <>
-        <h1>{"About Me"}</h1>
-        <p>{"Interested in software development, game development, cybersecurity, and Linux!"}</p>
-    </>
+        <>
+            <h1>{"About Me"}</h1>
+            <p>{"Interested in software development, game development, cybersecurity, and Linux!"}</p>
+        </>
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,15 @@ pub fn convert_list_uppercase(list: &Vec<&str>) -> Vec<Html> {
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
     html! {
-        <Link<Route> to={Route::About}>{ "About" }</Link<Route>>
+        <div class="navbar">
+            <ul>
+            // in reverse order
+                <li><Link<Route> to={Route::Contact}>{ "03. contact" }</Link<Route>></li>
+                <li><Link<Route> to={Route::Experience}>{ "02. experience" }</Link<Route>></li>
+                <li><Link<Route> to={Route::About}>{ "01. about" }</Link<Route>></li>
+                <li><code>{"[portfolio@storm ~]$"}</code></li>
+            </ul>
+        </div>
     }
 }
 
@@ -25,8 +33,8 @@ pub fn navbar() -> Html {
 pub fn app() -> Html {
     html! {
         <main>
-            <Navbar />
             <BrowserRouter>
+                <Navbar />
                 <Switch<Route>render={switch} />
             </BrowserRouter>
         </main>
