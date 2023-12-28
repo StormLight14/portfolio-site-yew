45683fb6945d29307f701c1ca85505a570a7aad5
Storm <stormpower44@protonmail.com>
2023-12-27 18:53:11 -0700

    Update router.rs

--- src/router.rs
+++ src/router.rs
@@ -1,10 +1,12 @@
-use yew_router::prelude::*;
+use crate::components::pages::{
+    about::About, contact::Contact, experience::Experience, projects::Projects,
+};
 use yew::prelude::*;
-use crate::components::pages::{about::About, contact::Contact, experience::Experience, projects::Projects};
+use yew_router::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, Routable)]
 pub enum Route {
-    #[at("/")]
+    #[at("/portfolio")]
     About,
     #[at("/experience")]
     Experience,
@@ -14,13 +16,11 @@ pub enum Route {
     Contact,
 }
 
-
 pub fn switch(route: Route) -> Html {
     match route {
-        Route::About => html!{<About />},
-        Route::Experience => html!{<Experience />},
-        Route::Projects => html!{<Projects />},
-        Route::Contact => html!{<Contact />},
+        Route::About => html! {<About />},
+        Route::Experience => html! {<Experience />},
+        Route::Projects => html! {<Projects />},
+        Route::Contact => html! {<Contact />},
     }
 }
-
